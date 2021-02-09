8303a9b9c2c27641d8836984544d2ea97b421fb4
lxshilaoda98 <n1n1n1_owner@163.com>
2021-02-09 17:28:06 +0800

    1

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const koa = require('koa');
 const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 const apiRouter = require('./routes');
-
+var Config = require('./config/config').getConfig('AppConfig');;
+const cors = require('koa2-cors');
 var http=require('http');
 
 const app = new koa();
 
-
+app.use(cors()) //解决跨域
 
 
 // 首页
@@ -26,8 +27,8 @@ app.use(apiRouter.routes());
 
 
 
-app.listen(3322,function () {
-    console.log("[报表系统]Start.. 应用实例[CallCenterReport]. 监听端口：", 3322)
+app.listen(Config.port,function () {
+    console.log("[报表系统]Start.. 应用实例[CallCenterReport]. 监听端口：", Config.port)
 });
 
 
--- config/config.js
+++ config/config.js
@@ -2,15 +2,8 @@
  * Created by Administrator on 2019/10/18.
  */
 const configParams = {
-    freeswitch : {
-        ip:'192.168.101.205',
-        port:8021,
-        password :'ClueCon',
-        APIkey:'FreeSWITCH', //获取API接口用到的key
-        RecordWav : '/home/RecordWav',//录音路径
-        RecordWavFTPIp : '192.168.101.193',
-        WebSocketLocalhost:'localhost',
-        WebSocketPort : '5555'//
+    AppConfig:{
+        port:3322,
     },
     database: {
         DATABASE: 'freeswitch',
